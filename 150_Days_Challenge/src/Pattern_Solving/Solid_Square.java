9a988921d44bb55c274528ec31cc8dcc093f629c
ADMIN <ADMIN@DESKTOP-G2SFRRL>
2026-08-21 07:48:10 +0530

    Initial Cimmit

---
+++ 150_Days_Challenge/src/Pattern_Solving/Solid_Square.java
@@ -0,0 +1,17 @@
+package Pattern_Solving;
+
+public class Solid_Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int row=5,col=5;
+		for(int i=1;i<=row;i++) {
+			for(int j=1;j<=col;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
